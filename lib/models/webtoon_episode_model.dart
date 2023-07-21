4895876a738774823fb9a31e8d1c3a5c27afdf2d
a17876 <a17876@hanmail.net>
2023-07-20 23:37:11 -0700

    add webtoonEpisodeModel

---
+++ lib/models/webtoon_episode_model.dart
@@ -0,0 +1,9 @@
+class WebtoonEpisodeModel {
+  final String title, rating, date, id;
+
+  WebtoonEpisodeModel.fromJson(Map<String, dynamic> json)
+      : id = json['id'],
+        title = json['title'],
+        rating = json['rating'],
+        date = json['date'];
+}
--- lib/services/api_service.dart
+++ lib/services/api_service.dart
@@ -1,5 +1,6 @@
 import 'dart:convert';
 import 'package:http/http.dart' as http;
+import '../models/webtoon_detail_model.dart';
 import '../models/webtoon_model.dart';
 
 class ApiService {
@@ -18,6 +19,17 @@ class ApiService {
         webtoonInstances.add(WebtoonModel.fromJson(webtoon));
       }
       return webtoonInstances;
+    }
+    throw Error();
+  }
+
+  static Future<WebtoonDetailModel> getToonById(String id) async {
+    final url = Uri.parse('$baseUrl/$id');
+    final response = await http.get(url);
+
+    if (response.statusCode == 200) {
+      final webtoon = jsonDecode(response.body);
+      return WebtoonDetailModel.fromJson(webtoon);
     }
     throw Error();
   }
